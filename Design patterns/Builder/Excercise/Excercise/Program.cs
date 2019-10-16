0fbd37df92ca8c0bb66162638eaf7b4c6581feb2
Andrii Dushkevych <adushkevych@logisense.com>
2019-10-16 15:17:28 -0400

    Build CodeBuilder which will generate new class code by sample call new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");

---
+++ Design patterns/Builder/Excercise/Excercise/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excercise
+{
+    public class CodeBuilder
+    {
+        public string ClassName;
+        public List<ClassField> Fields = new List<ClassField>();
+
+        public CodeBuilder(string className)
+        {
+            ClassName = className;
+        }
+
+        public CodeBuilder AddField(string name, string type)
+        {
+            Fields.Add(new ClassField(name, type));
+            return this;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"public class {ClassName}");
+            sb.AppendLine("{");
+            foreach (var classField in Fields)
+            {
+                sb.AppendLine(classField.ToString());
+            }
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+    }
+
+    public class ClassField
+    {
+        public string Name, Type;
+
+        public ClassField(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"  public {Type} {Name};";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeBuilder cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+            Console.ReadKey();
+        }
+    }
+}
