4912359021c854b342996558089316c0241667fd
Andrii Dushkevych <adushkevych@logisense.com>
2019-11-05 15:55:55 -0500

    Factory method added

---
+++ Factories/Factories/Factories/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class Point
+    {
+        public static Point CartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point PolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.PolarPoint(1.0, Math.PI / 2);
+            var point2 = Point.CartesianPoint(3, 15.2);
+            Console.WriteLine(point);
+            Console.WriteLine(point2);
+        }
+    }
+}
