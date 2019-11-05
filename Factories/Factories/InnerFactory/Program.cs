12615463d3badc329e806127cd6bbd852315f701
Andrii Dushkevych <adushkevych@logisense.com>
2019-11-05 16:25:03 -0500

    Inner factory added

---
+++ Factories/Factories/InnerFactory/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace InnerFactory
+{
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static class Factory
+        {
+            public static Point CartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point PolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+
+        public static Point Origin => new Point(0, 0);
+
+        public static Point OriginSingleInstance = new Point(0, 0);
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.Factory.PolarPoint(1, 1);
+            Console.WriteLine(point);
+        }
+    }
+}
