df2a7fd27265f12d2e8473273a92e87da3840031
Andrii Dushkevych <adushkevych@logisense.com>
2019-11-07 11:30:42 -0500

    Abstract factory added

---
+++ Factories/Factories/AbstractFactory/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AbstractFactory
+{
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Making a tea {amount} ml");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Making a coffee {amount} ml");
+            return new Coffee();
+        }
+    }
+
+    internal class Tea : IHotDrink {
+        public void Consume()
+        {
+            Console.WriteLine("Drinking tea");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Drinking coffee");
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        private List<Tuple<string, IHotDrinkFactory>> factories = new List<Tuple<string, IHotDrinkFactory>>();
+        public HotDrinkMachine()
+        {
+            foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
+            {
+                if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+                {
+                    factories.Add(Tuple.Create(
+                        t.Name.Replace("Factory", string.Empty),
+                        (IHotDrinkFactory) Activator.CreateInstance(t)
+                        ));
+                }
+            }
+        }
+
+        public IHotDrink MakeDrink()
+        {
+            Console.WriteLine("Available drinks: ");
+            for (int i = 0; i < factories.Count; i++)
+            {
+                var tuple = factories[i];
+                Console.WriteLine($"{i}: {tuple.Item1}");
+            }
+            Console.WriteLine("Choose a drink: ");
+            while (true)
+            {
+                string s;
+                if ((s = Console.ReadLine()) != null
+                    && int.TryParse(s, out int i)
+                    && i >= 0
+                    && i < factories.Count)
+                {
+                    Console.Write($"Specify the amount: ");
+                    s = Console.ReadLine();
+                    if (s != null && int.TryParse(s, out int amount) && amount > 0)
+                    {
+                        return factories[i].Item2.Prepare(amount);
+                    }
+                }
+
+                Console.WriteLine("Incorrect input. Try again");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink();
+        }
+    }
+}
