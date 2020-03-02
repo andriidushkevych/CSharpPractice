9fb699e889290db3069d2bf954da7faac1d12c8c
Andrii Dushkevych <adushkevych@logisense.com>
2020-03-02 18:22:10 -0500

    Monostate pattern added

---
+++ Singleton/Monostate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Monostate
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            var ceo = new CEO();
+            ceo.Name = "Adam";
+            ceo.Age = 55;
+
+            var ceo2 = new CEO();
+            Console.WriteLine(ceo2);
+        }
+    }
+}
